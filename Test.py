54fd114d8c10c63a3ce7c4fb206f975537af133f
metaron <wwzzyyzzrr@gmail.com>
2018-09-14 10:42:59 +0800

    test

--- Test.py
+++ Test.py
@@ -1,1 +1,14 @@
-import numpy as np
+class a:
+    def __init__(self):
+        mm = 0
+
+
+x=[]
+t = a()
+t.mm = 1
+x.append(t)
+for i in range(1,5):
+    x.append(a())
+    x[i].mm = x[i-1].mm+i
+for k in x:
+    print k.mm
