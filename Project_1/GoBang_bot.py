19c0b39e19afb6e8e5b32a22bf42a589589e4d3d
metaron <wwzzyyzzrr@gmail.com>
2018-09-20 23:09:14 +0800

    Update some code

--- Project_1/GoBang_bot.py
+++ Project_1/GoBang_bot.py
@@ -17,6 +17,8 @@ class AI(object):
         i = 0
         while (-1 < a + j < 15 and -1 < b + k < 15 and chessboard[a + j, b + k] == COLOR):
             i = i + 1
+            a = a + j
+            b = b + k
         if (-1 < a + j < 15 and -1 < b + k < 15 and chessboard[a + j, b + k] == COLOR_NONE):
             i = i * 2
         else:
@@ -34,7 +36,25 @@ class AI(object):
             x.append(self.count(chessboard,a,b,1,-1,COLOR))
             x.append(self.count(chessboard,a,b,-1,0,COLOR))
             x.append(self.count(chessboard,a,b,0,-1,COLOR))
+            y = []
             for i in range(0,3):
+                flag = x[i] + x[i+4]
+                if flag > 6:
+                    y.append(1)
+                elif flag == 6:
+                    if (x[i] * x[i + 4] < 0 or x[i] == x[i + 4] or (x[i] == 5 or x[i + 4] == 5)):
+                        y.append(1)#五连
+                    else:
+                        y.append(2)#活四
+                elif flag == 5:
+                    y.append(3)#冲四
+                elif flag == 4:
+                    if (x[i]*x[i+4] < 0):
+                        y.append()#死四
+
+                elif flag == 3:
+
+                elif flag == 2:
 
 
 
