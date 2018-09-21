fd3a369b351e475da36f465eed5279b5836455ca
Metaron <wwzzyyzzrr@gmail.com>
2018-09-21 13:40:56 +0800

    Update Pro1

--- Project_1/GoBang_bot.py
+++ Project_1/GoBang_bot.py
@@ -11,8 +11,7 @@ class AI(object):
         self.color = color
         self.chessboard_size = chessboard_size
         self.time_out = time_out
-        self.candidate_list = []
-
+        self.candidate_list = np.zeros((self.chessboard_size,self.chessboard_size))
     def count(self, chessboard, a, b, j, k, COLOR):
         i = 0
         while (-1 < a + j < 15 and -1 < b + k < 15 and chessboard[a + j, b + k] == COLOR):
@@ -27,6 +26,7 @@ class AI(object):
 
     def calcute_value(self, chessboard, a, b, COLOR):
         x = []
+        value = 0
         if chessboard[a, b] == 0:
             x.append(self.count(chessboard,a,b,1,1,COLOR))
             x.append(self.count(chessboard,a,b,-1,1,COLOR))
@@ -50,14 +50,51 @@ class AI(object):
                     y.append(3)#冲四
                 elif flag == 4:
                     if (x[i]*x[i+4] < 0):
-                        y.append()#死四
-
+                        y.append(8)#死四
+                    else:
+                        y.append(4)#活三
                 elif flag == 3:
-
+                    y.append(5)#冲三
                 elif flag == 2:
+                    if(x[i] == x[i + 4] or x[i] * x[i + 4] < 0):
+                        y.append(8)#死二死三
+                    else:
+                        y.append(6)#活二
+                elif flag == 1:
+                    y.append(7)#冲二
+                else:
+                    y.append(8)#死二等
+            z = [0,0,0,0,0,0,0,0,0]
+            if (y.__sizeof__()>0):
+                for m in y:
+                    z[m] = z[m]+1
+                if z[1] > 0:
+                    value = 30
+                elif z[2] > 0:
+                    value = 28
+                elif z[3] > 1:#双冲四
+                    value = 26
+                elif (z[3] > 0 and z[4] > 0):#四三
+                    value = 24
+                elif z[4] > 1:#双三
+                    value = 22
+                elif z[3] > 0:#冲四
+                    value = 20
+                elif (z[4] > 0 and z[5] > 0):#活三+冲三
+                    value = 18
+                elif z[4] > 0:#活三
+                    value = 16
+                elif z[5] > 0:#冲三
+                    value = 16 - 2 * z[5]
+                else:
+                    value = 2*(z[6]+z[7]+z[8])
+        return value
 
 
 
     def go(self, chessboard):
         COLOR = COLOR_BLACK
-        self.candidate_list.clear()
+        for a in range (0,self.chessboard_size):
+            for b in range (0,self.chessboard_size):
+                value = self.calcute_value(chessboard,a,b,COLOR)
+                if value > self.candidate_list(a,b):
