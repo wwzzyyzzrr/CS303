fe2a31e9924cce8472dcdfc0795116e3cfa953d7
wwzzyyzzrr <wwzzyyzzrr@gmail.com>
2018-11-12 12:14:57 +0800

    CS303

---
+++ Project_2/CARP.py
@@ -0,0 +1,27 @@
+import numpy  as np
+
+
+
+
+
+if __name__ == '__main__':
+    a = open("/home/metaron/文件/CS303/Project_2/Proj2_Carp/Proj2_Carp/CARP_samples/egl-e1-A.dat")
+    a.readline()
+    VERTICES = int(a.readline().split(' ').pop())
+    DEPOT = int(a.readline().split(' ').pop())
+    REdges = int(a.readline().split(' ').pop())
+    NREdges = int(a.readline().split(' ').pop())
+    VEHICLES = int(a.readline().split(' ').pop())
+    CAPACITY = int(a.readline().split(' ').pop())
+    TCORequired = int(a.readline().split(' ').pop())
+    a.readline()
+    line = a.readline()
+    edge = []
+    while (line != 'END'):
+        temp = (line.split(' '))
+        prop = []
+        for i in temp:
+            if i!='':
+                prop.append(int(i))
+        line = a.readline()
+    print(edge)
--- Project_2/test.py
+++ Project_2/test.py
@@ -1,2 +1,21 @@
-import pandas as pd
 import numpy  as np
+a = open("/home/metaron/文件/CS303/Project_2/Proj2_Carp/Proj2_Carp/CARP_samples/egl-e1-A.dat")
+a.readline()
+VERTICES = int(a.readline().split(' ').pop())
+DEPOT = int(a.readline().split(' ').pop())
+REdges = int(a.readline().split(' ').pop())
+NREdges = int(a.readline().split(' ').pop())
+VEHICLES = int(a.readline().split(' ').pop())
+CAPACITY = int(a.readline().split(' ').pop())
+TCORequired = int(a.readline().split(' ').pop())
+a.readline()
+line = a.readline()
+edge = []
+while (line != ''):
+    temp = (line.split(' '))
+    prop = []
+    for i in temp:
+        if i!=None:
+            prop.append()
+    line = a.readline()
+print(edge)
